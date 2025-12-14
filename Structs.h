e3a2f64793753e7edbaf959098a348071c549ef6
shobomaruXD <shimatomo1129@gmail.com>
2025-12-14 18:00:12 +0900

    コメント更新

--- Structs.h
+++ Structs.h
@@ -14,17 +14,17 @@ typedef struct{
     FILE *file;
 }VMWriter;
 
-typedef struct Symbol{
+typedef struct Symbol{  //一つの変数の情報を保持するnode
     char name[N];
     char type[N];
     char kind[N];     // 種類(static,field,argument,var)
     int index;      //VMセグメントでのインデックス(start from 0)
-    struct Symbol *next; // ハッシュ衝突のためのリンク
+    struct Symbol *next; // ハッシュ衝突解決のためのリンク(ポインタ)
 }Symbol;
 
 typedef struct SymbolTable{     // シンボルテーブル全体を管理する構造体
-    Symbol *classScope[HASH_SIZE]; 
-    Symbol *subroutineScope[HASH_SIZE];
+    Symbol *classScope[HASH_SIZE];      //ハッシュ配列
+    Symbol *subroutineScope[HASH_SIZE]; //ハッシュ配列
 
     int staticCount;
     int fieldCount;
