391a30a2b814fb8a99fb7970c7c880a62ad788b1
shobomaruXD <shimatomo1129@gmail.com>
2025-12-14 17:18:33 +0900

    fixed SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include "Structs.h"
 
-LocalCounter *startSubroutine();
-void defineArgVars(LocalCounter *LC,char varName[],Vars *varStorage[]);
+SymbolTable *createSymbolTable();
+void startSubroutine(SymbolTable *table);
 
 #endif
