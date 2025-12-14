327c8b5cd511626f44d80ea8e105830f12a1f39d
shobomaruXD <shimatomo1129@gmail.com>
2025-12-14 17:28:26 +0900

    added define function

--- SymbolTable.c
+++ SymbolTable.c
@@ -13,3 +13,16 @@ void startSubroutine(SymbolTable *table){   // initing subroutine table
     table->argCount=0;
     table->varCount=0;
 }
+
+void define(SymbolTable *table,const char *name,const char *type,const char *kind){
+    int index=0;
+    if(strcmp(kind,"static")==0){
+        index=table->staticCount++; //classScopeに登録
+    }else if(strcmp(kind,"field")==0){
+        index=table->fieldCount++;  //classScopeに登録
+    }else if(strcmp(kind,"argument")==0){
+        index=table->argCount++;    //subroutineScopeに登録
+    }else if(strcmp(kind,"var")==0){
+        index=table->varCount++;    //subroutineScopeに登録
+    }
+}
